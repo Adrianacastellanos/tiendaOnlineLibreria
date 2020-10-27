b90302ee42eba461d89b68f607d227ab4cf55384
Eduardo <dardo1818@gmail.com>
2020-10-26 20:55:55 -0600

    Anadir carrito anade items
    Solamente anade la cantidad de libros y su total, esta funcionalidad solo esta en 1984.html, falta en las demas paginas

---
+++ js/funciones.js
@@ -0,0 +1,28 @@
+
+var price_int = 0;
+function computePrice() {
+    var els = document.getElementsByTagName('span');
+
+    let prop;
+    // Get each attribute from a span and obtain the attribute from itemprop.
+    for (var i = 0; i < els.length; i++) {
+        prop = els[i].getAttribute('itemprop');
+
+        if (prop) {
+            price = els[i].innerHTML;
+            break;
+        }
+    }
+    price = price.replace('$','');  // Delete $.
+    price_int += parseFloat(price); // Just add the price.
+}
+
+var amount = 0;
+function buy() {
+    amount += 1;
+    document.getElementById("amount").innerHTML = amount;
+
+    computePrice();
+    document.getElementById("price").innerHTML = price_int;
+}
+
